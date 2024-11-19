40f0910d5771f229a6603e6bb14070256054dc84
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2024-11-19 21:03:04 +0900

    feat: 페이지별 링크 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { Login, Read, Write } from "./pages";
 
 function App() {
@@ -7,12 +7,15 @@ function App() {
     <div className="App">
       <header>
         <h1>여기에 네비게이션 바 추가하기</h1>
+        <Link to="/login">로그인</Link>
+        <Link to="/read">글 목록 보기</Link>
+        <Link to="/write">글쓰기</Link>
       </header>
-      {/* <Routes>
+      <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/write" element={<Write />} />
         <Route path="/read" element={<Read />} />
-      </Routes> */}
+      </Routes>
       <footer>
         <p>여기에 푸터 추가하기</p>
       </footer>
