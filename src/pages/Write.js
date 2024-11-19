441878c9415f866192e67cb062405579e3c88b6f
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2024-11-19 21:27:09 +0900

    layout: 글 제목 인풋, 저장 버튼

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -5,6 +5,41 @@ import "react-quill/dist/quill.snow.css";
 function Write() {
   return (
     <div>
+      <div>
+        글작성용 상단바
+        <button
+          class="button"
+          aria-busy="false"
+          role="button"
+          tabindex="0"
+          aria-pressed="false"
+          type="button"
+        >
+          <div class="buttontext">저장</div>
+        </button>
+        <button
+          class="button"
+          aria-busy="false"
+          role="button"
+          tabindex="0"
+          aria-pressed="false"
+          type="button"
+        >
+          <div class="buttontext">게시</div>
+        </button>
+      </div>
+      <input
+        class="title1"
+        maxlength="100"
+        placeholder="제목을 입력하세요"
+        type="text"
+      ></input>
+      <input
+        class="title2"
+        maxlength="100"
+        placeholder="부제목을 입력하세요"
+        type="text"
+      ></input>
       <MyComponent />
     </div>
   );
