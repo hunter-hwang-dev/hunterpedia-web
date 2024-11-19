e753e33eaa974c741795e5b49b86a0323b9adcfd
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2024-11-19 21:18:14 +0900

    feat: react-quill 샘플 코드 추가

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,8 +1,25 @@
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
 function Write() {
   return (
     <div>
-      <div>Write Page</div>
+      <MyComponent />
     </div>
+  );
+}
+
+function MyComponent() {
+  const [value, setValue] = useState("");
+
+  return (
+    <ReactQuill
+      theme="snow"
+      style={{ height: "600px" }}
+      value={value}
+      onChange={setValue}
+    />
   );
 }
 
